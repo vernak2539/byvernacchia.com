bcb9e5cd98cca4abc8a4242f86db9984b3982b10
Alex Vernacchia <vernak2539@users.noreply.github.com>
2025-08-27 10:49:57 -0400

    remove things and update svg

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-import tippy from "tippy.js";
-import setupSvg from "./src/setupSvg";
 import initTyping from "./src/initTyping";
 
 import "normalize.css";
-import "tippy.js/dist/tippy.css";
 import "./src/styles.css";
 
-tippy("[data-tippy-content]");
 
-setupSvg();
 initTyping();
